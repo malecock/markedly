3273a36eca89bf4783e979e885665642b17ac1ff
malecock <mattalecock@gmail.com>
2014-01-06 00:42:53 -0800

    production version

--- app.rb
+++ app.rb
@@ -6,9 +6,11 @@ require 'rubygems'
 require 'data_mapper'
 require './sign_up'
 require './helpers.rb'
+require 'securerandom'
+
+enable :sessions
 
 puts "This is process #{Process.pid}"
-
 get ('/markedly_styles.css'){ scss :markedly_styles}
 
 get '/' do
@@ -16,12 +18,14 @@ get '/' do
 	slim :home
 end
 
+
 post '/sign_up' do
+	session[:secret]
 	if params[:url].include? "http://"
 		url = params[:url]
 	else
 		url = params[:url].prepend("http://")
-	end
+	end	
 
 	new_record = Sign_up.create(
 		:firstname => params[:firstname], 
@@ -30,22 +34,28 @@ post '/sign_up' do
 		:email => params[:email], 
 		:url => url)
 
-	@record_saved = new_record.saved?
-	form_errors = new_record.errors
-
-	puts @record_saved
-
-	form_errors.each do |form_error| 
-		 puts form_error
+	if new_record.saved? then
+		#{}"firstname=#{params[:firstname]}|lastname=#{params[:lastname]}|phone=#{params[:phone]}|email=#{params[:email]}|url=#{params[:url]}"
+		@record_saved = new_record.saved?
+		security_check = SecureRandom.hex 
+		puts @security_check
+		redirect to("/confirm?val=#{security_check}")
+	else
+		new_record.errors.each do 
+			|form_error| puts form_error 
+			redirect to('/') 
+		end
 	end
-
-	redirect to('/')
 end
 
-get '/admin' do
-	#for retrieving form submissions
-	protected!
-	slim :admin
+get '/confirm' do
+	#check = params[:security_check].to_s
+		@sign_up_firstname = Sign_up.last[:firstname]
+		@sign_up_lastname = Sign_up.last[:lastname]
+		@sign_up_url = Sign_up.last[:url]
+		@sign_up_email = Sign_up.last[:email]
+		@sign_up_phone = Sign_up.last[:phone]
+	slim :confirm
 end
 
 not_found do
@@ -58,4 +68,3 @@ error do
 	#for 500 status
 	slim :error
 end
-
